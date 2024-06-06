13ff0ea62832d0f0c16964e7816579c28b3d091c
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2024-06-06 13:57:49 +0530

    Add files via upload

---
+++ Algorithm_Visualizer/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import './App.css';
+import Sorting from './components/Sorting';
+import Graph from './components/Graph';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){  
+      setMode('dark');
+      document.body.style.backgroundColor = 'gray';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+  return (
+    <>
+    <Router>
+      <Navbar mode={mode} toggleMode={toggleMode}/>
+      <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/Sorting_Vis" element={<Sorting mode={mode}/>}/>
+          <Route exact path="/Graph_Vis" element={<Graph mode={mode}/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
