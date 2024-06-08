91868e4d56dc47eed340111452368200f48cafa3
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2024-06-08 22:06:01 +0530

    Update Navbar.js

--- Algorithm_Visualizer/src/components/Navbar.js
+++ Algorithm_Visualizer/src/components/Navbar.js
@@ -8,15 +8,20 @@ export default function Navbar(props) {
         <Link className="navbar-brand" to="/">Visualizer</Link>
         <div className="navbar-collapse">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link active" to="/Sorting_Vis">Sorting Visualizer</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link active" to="/Graph_Vis">Graph Visualizer</Link>
-                </li>
+              <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              <div className="dropdown">
+                <button className={`btn btn-${props.mode} dropdown-toggle`} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Select Algo
+                </button>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" to="/Sorting_Vis">Sorting Visualizer</Link></li>
+                  <li><Link className="dropdown-item" to="/Graph_Vis">Graph Visualizer</Link></li>
+                  <li><Link className="dropdown-item" to="/Search_Vis">Searching Visualizer</Link></li>
+                  <li><Link className="dropdown-item" to="/SievePrime_Vis">Sieve of Eratosthenes</Link></li>
+                </ul>
+              </div>
             </ul>
         </div>
         <div className="form-check-reverse form-switch">
