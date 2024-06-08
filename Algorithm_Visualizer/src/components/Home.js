4c37e752db85aa36568c424e2bfa05ed832c8fa0
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2024-06-08 22:05:12 +0530

    Update Home.js

--- Algorithm_Visualizer/src/components/Home.js
+++ Algorithm_Visualizer/src/components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import sorting_img from './sorting_img.jpeg';
-import graph_img from './graph_img.png';
+import graph_img from './graph_img.webp';
+import search_img from './search_img.avif';
+import sieve_img from './sieve_img.jpeg';
 
 export default function Home() {
   return (
     <>
         <div className="container my-5 d-flex justify-content-evenly">
-          <Link to="Sorting_Vis"><img src={sorting_img} alt="Sorting_img" style={{height:"300px", width:"100%"}}/></Link>
-          <Link to="Graph_Vis"><img src={graph_img} alt="Graph_img" style={{height:"300px", width:"100%"}}/></Link>
+          <Link to="Sorting_Vis"><img src={sorting_img} alt="Sorting_img" style={{height:"250px", width:"90%"}}/></Link>
+          <Link to="Graph_Vis"><img src={graph_img} alt="Graph_img" style={{height:"250px", width:"90%"}}/></Link>
+          <Link to="Search_Vis"><img src={search_img} alt="Search_img" style={{height:"250px", width:"90%"}}/></Link>
+          <Link to="SievePrime_Vis"><img src={sieve_img} alt="Sieve_img" style={{height:"250px", width:"90%"}}/></Link>
         </div>
     </>
   )
